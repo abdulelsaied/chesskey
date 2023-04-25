be1e39efc8a9a66fe7f4912dff208372c8be2438
Abdul El-Saied <abdul.elsaied@berkeley.edu>
2023-04-25 13:35:21 -0700

    add connect/disconnect fix

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import random
+import time
 from flask import Flask, render_template, redirect, url_for, request, session
 from flask_socketio import SocketIO, send, emit, join_room, leave_room
 from db import *
@@ -19,22 +21,141 @@ def index():
 
     :return: None
     """
-    # if request.method != 'POST':
-    #     return render_template('index.html')
-    # else:
-    #     if 'createSubmit' in request.form:
-    #         room_name = 
+    if request.method == 'POST':
+        if 'createSubmit' in request.form:
+
+            # check if the room already exists in the database
+            room_input = request.form.get("lobby")
+            if db.session.query(Room).filter_by(room_name = room_input).first(): 
+                return render_template("index.html")
+
+            # create room from form fields
+            # data:
+            # room name
+            # host username 
+            # opp username None
+            # side
+            # time control
+            # increment
+            # current score
+            
+            username = request.form.get("username")
+            side = request.form.get("btnradio")
+            if side == "random":
+                side = random.choice(["white", "black"])
+            time_control = request.form.get("time-control")
+            increment = request.form.get("increment")
+            room = Room(
+                room_name = room_input,
+                host_username = username,
+                opp_username = None
+            )
+            db.session.add(room)
+            db.session.commit()
+
+            session['username'] = username
+            return redirect(url_for("create_lobby", lobby = room_input)) 
+        elif 'joinSubmit' in request.form:
+            room_input = request.form.get("lobby")
+            username = request.form.get("username")
+            session['username'] = username
+            room_row = db.session.query(Room).filter_by(room_name = room_input).first()
+            if not room_row or room_row.opp_username:
+                return render_template("index.html")
+            room_row.opp_username = username
+            db.session.commit()
+            return redirect(url_for("create_lobby", lobby = room_input))
+    else:
+        return render_template("index.html")
+
+@app.route("/<lobby>", methods = ['GET', 'POST'])
+def create_lobby(lobby):
+    """
+    creates a lobby and routes the user to that lobby.
+    if a session for the user already exists, route them to the lobby 
+    if user is a new opponent, retrieve the form data and add it to the db 
+
+    :param lobby: unique string for a chess lobby
+    :return: None
+    """
+    room_row = db.session.query(Room).filter_by(room_name = lobby).first()
+    if not room_row:
+        return render_template("index.html")
+    
+    data = {'username': None, 'lobby': lobby, 'modal': False}
+
+    if request.method == 'POST':
+        data['username'] = request.form.get('username')
+        session['username'] = data['username']
+        room_row.opp_username = data['username']
+        db.session.commit()
+        return render_template("room.html", data = data)
+    if session.get("username"):
+        data["username"] = session.get("username")
+        if session.get("username") == room_row.host_username:
+            return render_template("room.html", data = data)
+        elif session.get("username") == room_row.opp_username:
+            return render_template("room.html", data = data)
+        elif not room_row.opp_username:
+            room_row.opp_username = session.get("username")
+            db.session.commit()
+            return render_template("room.html", data = data)
+        else:
+            return render_template("index.html")
+    else:
+        data["modal"] = True
+        return render_template("room.html", data = data)
+
+@sio.event
+def incoming_msg(data):
+    """
+    broadcasts message to all users in the desired room, with a timestamp of when they sent it
+
+    :param data: a dict in the form (username, room, message)
+    :return: None
+    """
+    username = data["username"]
+    room = data["room"]
+    msg = data["msg"]
+    time_stamp = time.strftime('%b-%d %I:%M%p', time.localtime())
+    send({"username": username, "msg": msg, "time_stamp": time_stamp}, to = room)
+
+
+@sio.event
+def join(data):
+    """
+    connects a user to the desired room, and broadcasts that they connected to all users of the room
+
+    :param data: a dict in the form (username, room)
+    :return: None
+    """
+    if data["username"] is not None:
+        username = data["username"]
+        room = data["room"]
+        join_room(room)
+        time_stamp = time.strftime('%b-%d %I:%M%p', time.localtime())
+        sio.emit('incoming-status-msg', {"msg": username + " has joined room " + room, 'time_stamp': time_stamp}, to = room)
+    
+@sio.event
+def leave(data):
+    """
+    disconnects a user from the desired room, and broadcasts that they disconnected to all users of the room
+
+    :param data: a dict in the form (username, room)
+    :return: None
+    """
+    username = data["username"]
+    room = data["room"]    
+    time_stamp = time.strftime('%b-%d %I:%M%p', time.localtime())
+    sio.emit('incoming-status-msg', {"msg": username + " has left room " + room, 'time_stamp': time_stamp}, to = room)
+    leave_room(room)
+
 
 @sio.event
 def connect():
     print(request.sid, ' connected')
-    room = Room(
-        room_name = "test room",
-        host_username = "test username",
-        opp_username = None
-    )
-    db.session.add(room)
-    db.session.commit()
+    ## EMIT HERE
+
 
 @sio.event
 def disconnect():
--- static/scripts/socketio.js
+++ static/scripts/socketio.js
@@ -1,9 +1,127 @@
-const sio = io();
+import { Chess } from '../node_modules/chess.js/dist/chess.js'
 
-sio.on('connect', () => {
-    console.log('connected');
-})
+document.addEventListener('DOMContentLoaded', () => {
 
-sio.on('disconnect', () => {
-    console.log('disconnected');
+    var btn = document.getElementById("usernameButton");
+    if (btn.value == "False") {
+    
+      const socket = io();
+      const username = document.querySelector('#username').getAttribute('data-value');
+      const room = document.querySelector('#lobby').getAttribute('data-value');
+
+      socket.on('connect', () => {
+          console.log("connected");
+          socket.emit('join', {'username': username, 'room': room});
+      });
+
+      // when a user disconnects, its crucial to delete the row from the database and alert the remaining player that the room is now closed
+      socket.on('disconnect', () => {
+        console.log("disconnected");
+        // socket.emit('leave', {'username': username, 'room': room});
+    });
+
+      socket.on('incoming-status-msg', data => {
+          if (data['msg']) {
+              const p = document.createElement('p');
+              p.innerHTML = "[" + data['time_stamp'] + "] " + data['msg']
+              document.querySelector('#display-message-section').append(p);
+          }
+      });
+
+      document.querySelector('#send_message').onclick = () => {
+          socket.emit('incoming_msg', {'msg': document.querySelector('#user_message').value, 'username': username, 'room': room});
+          document.querySelector('#user_message').value = '';
+      }
+
+      socket.on('message', data => {
+          if (data['msg']){
+              const p = document.createElement('p');
+              const br = document.createElement('br');
+              p.innerHTML = "[" + data['time_stamp'] + "] " + data['username'] + ": " + data['msg'];
+              document.querySelector('#display-message-section').append(p);
+          }
+      });
+
+      var board = null
+      var game = new Chess()
+      var $status = $('#status')
+      var $fen = $('#fen')
+      var $pgn = $('#pgn')
+      
+      function onDragStart (source, piece, position, orientation) {
+        // do not pick up pieces if the game is over
+        if (game.isGameOver()) return false
+      
+        // only pick up pieces for the side to move
+        if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+            (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+          return false
+        }
+      }
+      
+      function onDrop (source, target) {
+        // see if the move is legal
+        var move = game.move({
+          from: source,
+          to: target,
+          promotion: 'q' // NOTE: always promote to a queen for example simplicity
+        })
+      
+        // illegal move
+        if (move === null) return 'snapback'
+      
+        updateStatus()
+      }
+      
+      // update the board position after the piece snap
+      // for castling, en passant, pawn promotion
+      function onSnapEnd () {
+        board.position(game.fen())
+      }
+      
+      function updateStatus () {
+        var status = ''
+      
+        var moveColor = 'White'
+        if (game.turn() === 'b') {
+          moveColor = 'Black'
+        }
+      
+        // checkmate?
+        if (game.isCheckmate()) {
+          status = 'Game over, ' + moveColor + ' is in checkmate.'
+        }
+      
+        // draw?
+        else if (game.isDraw()) {
+          status = 'Game over, drawn position'
+        }
+      
+        // game still on
+        else {
+          status = moveColor + ' to move'
+      
+          // check?
+          if (game.inCheck()) {
+            status += ', ' + moveColor + ' is in check'
+          }
+        }
+      
+        $status.html(status)
+        $fen.html(game.fen())
+        $pgn.html(game.pgn())
+      }
+      
+      var config = {
+        draggable: true,
+        position: 'start',
+        onDragStart: onDragStart,
+        onDrop: onDrop,
+        onSnapEnd: onSnapEnd
+      }
+
+      board = Chessboard('myBoard', config)
+      
+      updateStatus()
+    }
 })
